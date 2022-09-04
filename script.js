b57f73606d9079c177691f4af4bfa319cd5f9ee5
ayodeji <87223209+Ayodeji-Oloja@users.noreply.github.com>
2022-09-04 17:26:24 +0100

    Added HTML, CSS Files, & Media Assets

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "72%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
